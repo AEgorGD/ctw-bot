70b6743087640b948c30d8bdda0862374f984aa0
AEgorGD <aegor.play@gmail.com>
2026-07-08 19:30:10 +0300

    Create brawl.py

---
+++ brawl.py
@@ -0,0 +1,68 @@
+import requests
+
+from config import BRAWL_TOKEN
+
+
+HEADERS = {
+    "Authorization": f"Bearer {BRAWL_TOKEN}"
+}
+
+
+CLUB_TAG = "#ТЕГ_КЛУБА"
+
+
+def get_club_players():
+
+    url = (
+        "https://api.brawlstars.com/v1/clubs/"
+        f"{CLUB_TAG.replace('#', '%23')}"
+    )
+
+
+    response = requests.get(
+        url,
+        headers=HEADERS
+    )
+
+
+    data = response.json()
+
+
+    players = []
+
+
+    for member in data["members"]:
+
+        tag = member["tag"]
+
+
+        player_url = (
+            "https://api.brawlstars.com/v1/players/"
+            f"{tag.replace('#', '%23')}"
+        )
+
+
+        player = requests.get(
+            player_url,
+            headers=HEADERS
+        ).json()
+
+
+        players.append({
+
+            "name": player["name"],
+
+            "tag": tag,
+
+            "trophies": player["trophies"]
+
+        })
+
+
+    players.sort(
+        key=lambda x: x["trophies"],
+        reverse=True
+    )
+
+
+    return players
